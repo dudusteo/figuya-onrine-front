3e277bd29d90e24eb4b97de5b7011da926e5c809
DuDuSteo <p.aleksiejuk@protonmail.com>
2024-09-04 11:50:49 +0200

    Use mui template for cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,21 +1,40 @@
 import * as React from "react";
 
-import { Box, CssBaseline, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Grid, Link, Stack, Step, StepLabel, Stepper, Typography } from "@mui/material";
 
-import PurchaseSummary from "./PurchaseSummary";
 import CartService from "../../services/cartService";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { IOrder, RelationType } from "@spree/storefront-api-v2-sdk/dist/*";
+import { IOrder } from "@spree/storefront-api-v2-sdk/dist/*";
 import {
 	getOrderToken,
 	setOrderToken,
 } from "../../features/token/orderTokenSlice";
-import CartProduct from "./CartProduct";
 import { getCart, updateOrder } from "../../features/basket/basketSlice";
 import { useTranslation } from "react-i18next";
+import AddressForm from "./address-form";
+import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import figuya_logo from "../../assets/figuya_logo.svg";
+import Info from "./info";
+
+function getStepContent(step: number) {
+	switch (step) {
+		case 0:
+			return <AddressForm />;
+		// case 1:
+		// 	return <PaymentForm />;
+		// case 2:
+		// 	return <Review />;
+		default:
+			throw new Error('Unknown step');
+	}
+}
+
+const steps = ['Shipping address', 'Payment details', 'Review your order'];
 
 const Cart = () => {
 	const { t } = useTranslation();
+	const [activeStep, setActiveStep] = React.useState(0);
 	const orderToken = useAppSelector(getOrderToken);
 	const cart = useAppSelector(getCart)
 	const dispatch = useAppDispatch();
@@ -34,56 +53,222 @@ const Cart = () => {
 		}
 	}, [orderToken, dispatch]);
 
+	const handleNext = () => {
+		setActiveStep(activeStep + 1);
+	};
+	const handleBack = () => {
+		setActiveStep(activeStep - 1);
+	};
+
 	if (!cart || !orderToken) {
 		return <div>Loading...</div>;
 	}
 
 	return (
-		<Box
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				minHeight: "100vh",
-				px: 10,
-				py: 2,
-			}}
-		>
-			<CssBaseline />
-
-			<Typography
-				variant="h4"
+		<Grid container sx={{ height: { xs: '100%', sm: '100dvh' } }}>
+			<Grid item
+				sm={12}
+				md={7}
+				lg={8}
 				sx={{
-					color: "primary.main",
-					fontWeight: "bold",
+					display: 'flex',
+					flexDirection: 'column',
+					maxWidth: '100%',
+					width: '100%',
+					backgroundColor: { xs: 'transparent', sm: 'background.default' },
+					alignItems: 'start',
+					pt: { xs: 6, sm: 16 },
+					px: { xs: 2, sm: 10 },
+					gap: { xs: 4, md: 8 },
 				}}
 			>
-				{t("cart.cart-content")}
-			</Typography>
-
-			<Stack spacing={2} sx={{ my: 2 }}>
-				{Array.isArray(cart?.data.relationships.variants.data) &&
-					Array.isArray(cart?.data.relationships.line_items.data) &&
-					cart.data.relationships.variants.data.map(
-						(product: RelationType, key: number) => (
-							<CartProduct
-								productId={product.id}
-								lineItemId={
-									// @ts-ignore
-									cart.data.relationships.line_items.data[key].id
-								}
-								orderToken={orderToken}
-								key={key}
-							/>
-						)
+				<Box
+					sx={{
+						display: 'flex',
+						justifyContent: { sm: 'space-between', md: 'flex-end' },
+						alignItems: 'center',
+						width: '100%',
+						maxWidth: { sm: '100%', md: 600 },
+					}}
+				>
+					<Box
+						sx={{
+							display: { xs: 'none', md: 'flex' },
+							flexDirection: 'column',
+							justifyContent: 'space-between',
+							alignItems: 'flex-end',
+							flexGrow: 1,
+						}}
+					>
+						<Stepper
+							id="desktop-stepper"
+							activeStep={activeStep}
+							sx={{ width: '100%', height: 40 }}
+						>
+							{steps.map((label) => (
+								<Step
+									sx={{ ':first-child': { pl: 0 }, ':last-child': { pr: 0 } }}
+									key={label}
+								>
+									<StepLabel>{label}</StepLabel>
+								</Step>
+							))}
+						</Stepper>
+					</Box>
+				</Box>
+				<Card sx={{ display: { xs: 'flex', md: 'none' }, width: '100%' }}>
+					<CardContent
+						sx={{
+							display: 'flex',
+							width: '100%',
+							alignItems: 'center',
+							justifyContent: 'space-between',
+						}}
+					>
+						<div>
+							<Typography variant="subtitle2" gutterBottom>
+								Selected products
+							</Typography>
+							<Typography variant="body1">
+								{activeStep >= 2 ? '$144.97' : '$134.98'}
+							</Typography>
+						</div>
+						{/* <InfoMobile totalPrice={activeStep >= 2 ? '$144.97' : '$134.98'} /> */}
+					</CardContent>
+				</Card>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						flexGrow: 1,
+						width: '100%',
+						maxWidth: { sm: '100%', md: 600 },
+						maxHeight: '720px',
+						gap: { xs: 5, md: 'none' },
+					}}
+				>
+					<Stepper
+						id="mobile-stepper"
+						activeStep={activeStep}
+						alternativeLabel
+						sx={{ display: { sm: 'flex', md: 'none' } }}
+					>
+						{steps.map((label) => (
+							<Step
+								sx={{
+									':first-child': { pl: 0 },
+									':last-child': { pr: 0 },
+									'& .MuiStepConnector-root': { top: { xs: 6, sm: 12 } },
+								}}
+								key={label}
+							>
+								<StepLabel
+									sx={{ '.MuiStepLabel-labelContainer': { maxWidth: '70px' } }}
+								>
+									{label}
+								</StepLabel>
+							</Step>
+						))}
+					</Stepper>
+					{activeStep === steps.length ? (
+						<Stack spacing={2} useFlexGap>
+							<Typography variant="h1">📦</Typography>
+							<Typography variant="h5">Thank you for your order!</Typography>
+							<Typography variant="body1" sx={{ color: 'text.secondary' }}>
+								Your order number is
+								<strong>&nbsp;#140396</strong>. We have emailed your order
+								confirmation and will update you once its shipped.
+							</Typography>
+							<Button
+								variant="contained"
+								sx={{ alignSelf: 'start', width: { xs: '100%', sm: 'auto' } }}
+							>
+								Go to my orders
+							</Button>
+						</Stack>
+					) : (
+						<React.Fragment>
+							{getStepContent(activeStep)}
+							<Box
+								sx={[
+									{
+										display: 'flex',
+										flexDirection: { xs: 'column-reverse', sm: 'row' },
+										alignItems: 'end',
+										flexGrow: 1,
+										gap: 1,
+										pb: { xs: 12, sm: 0 },
+										mt: { xs: 2, sm: 0 },
+										mb: '60px',
+									},
+									activeStep !== 0
+										? { justifyContent: 'space-between' }
+										: { justifyContent: 'flex-end' },
+								]}
+							>
+								{activeStep !== 0 && (
+									<Button
+										startIcon={<ChevronLeftRoundedIcon />}
+										onClick={handleBack}
+										variant="text"
+										sx={{ display: { xs: 'none', sm: 'flex' } }}
+									>
+										Previous
+									</Button>
+								)}
+								{activeStep !== 0 && (
+									<Button
+										startIcon={<ChevronLeftRoundedIcon />}
+										onClick={handleBack}
+										variant="outlined"
+										fullWidth
+										sx={{ display: { xs: 'flex', sm: 'none' } }}
+									>
+										Previous
+									</Button>
+								)}
+								<Button
+									variant="contained"
+									endIcon={<ChevronRightRoundedIcon />}
+									onClick={handleNext}
+									sx={{ width: { xs: '100%', sm: 'fit-content' } }}
+								>
+									{activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+								</Button>
+							</Box>
+						</React.Fragment>
 					)}
-			</Stack>
-
-			<PurchaseSummary
-				itemCost={cart.data.attributes.display_item_total}
-				shipmentCost={cart.data.attributes.display_ship_total}
-				totalCost={cart.data.attributes.display_total}
-			/>
-		</Box>
+				</Box>
+			</Grid>
+			<Grid item
+				xs={12}
+				sm={5}
+				lg={4}
+				sx={{
+					display: { xs: 'none', md: 'flex' },
+					flexDirection: 'column',
+					backgroundColor: 'background.paper',
+					borderLeft: { sm: 'none', md: '1px solid' },
+					borderColor: { sm: 'none', md: 'divider' },
+					alignItems: 'start',
+					pt: 16,
+					px: 10,
+					gap: 4,
+				}}
+			>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						flexGrow: 1,
+						width: '100%',
+						maxWidth: 500,
+					}}
+				>
+					<Info totalPrice={activeStep >= 2 ? '$144.97' : '$134.98'} />
+				</Box>
+			</Grid>
+		</Grid>
 	);
 };
 
